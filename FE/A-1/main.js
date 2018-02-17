39782cc22eff67f45ebde398a747d09e63df4a85
Ramasubbaiya Adaikkalam <ramasubbaiya@gmail.com>
2018-02-17 12:45:31 -0500

    Added comments

--- FE/A-1/main.js
+++ FE/A-1/main.js
@@ -1,16 +1,27 @@
-function isPrimeNumber() {
+// check button
+function checkMe() {
     let input = document.getElementById('inputNumber').value;
-    let isPrime = false;
     let outputText = "Please enter a valid number!";
 
-    if (input && input > 0) {
-        for (let i = 2; i < (input / 2); i++) {
-            if (input % i == 0) {
-                isPrime = true;
-            }
-        }
-        outputText = isPrime ? 'It is a Prime Number' : 'It is not a Prime Number';
+    // check input is greater than zero, if not ask to reenter the number
+    if (input > 0) {
+        outputText = (isPrimeNumber(input)) ? 'It is a Prime Number' : 'It is not a Prime Number';
     }
 
+    // show the result
     document.getElementsByTagName('h3')[0].innerHTML = outputText;
+}
+
+// isPrimeNumber gets a number greater than zero as an arguments, returns true when the input is a prime number else it is false
+function isPrimeNumber(input) {
+
+    if (input < 2) return false;
+
+    for (let i = 2; i <= (input / 2); i++) {
+        if (input % i == 0) {
+            return false;
+        }
+    }
+
+    return true;
 }
