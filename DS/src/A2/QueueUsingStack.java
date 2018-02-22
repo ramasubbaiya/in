8dd7d06dd10be4b5c355d3ad1409b8ff3c250449
Ramasubbaiya Adaikkalam <ramasubbaiya@gmail.com>
2018-02-21 23:42:54 -0500

    Added comments

--- DS/src/A2/QueueUsingStack.java
+++ DS/src/A2/QueueUsingStack.java
@@ -1,27 +1,40 @@
 package A2;
 
+/**
+ * Implementing Queue using Two Stacks
+ * @author Rama
+ *
+ */
 public class QueueUsingStack {
 
 	Stack s1 = null;
 	Stack s2 = null;
+	
+	// size of the Stacks
 	int size = 0;
 	
+	// constructor with two stacks
 	public QueueUsingStack(int size) {
 		this.s1 = new Stack(size);
 		this.s2 = new Stack(size);
 	}
-	public void enqueue(int x) {
-		this.s1.push(x);
-	}
 	
+	// insert value to the Queue
+	public void enqueue(int x) {
+		s1.push(x);
+	}
+	// get the first value from the Queue	
 	public int dequeue() {
-		while(!s1.isEmpty()) {
-			this.s2.push(this.s1.pop());
+		if(s2.isEmpty()) {
+			while(!s1.isEmpty()) {
+				s2.push(s1.pop());
+			}
 		}
 		return this.s2.pop();
 	}
 	
 	public static void main(String[] args) {
+		// Testing Queue using Stack with inserting three values
 		QueueUsingStack q = new QueueUsingStack(10);
 		q.enqueue(1);
 		q.enqueue(2);
--- DS/src/A2/Stack.java
+++ DS/src/A2/Stack.java
@@ -1,39 +1,48 @@
 package A2;
 
+/**
+ * Implementing Stack DS
+ * @author Rama
+ */
 public class Stack {
 
-	int size = 0;
-	int count = -1;
-	int[] stackArr = null;
+	int size = 0; // Size of the stack
+	int count = -1; // To hold the pointer for the current value and measure the size of the stack
+	int[] stackArr = null; // Initialize stack array
 	
+	// Constructor with the size
 	public Stack(int size) {
 		stackArr = new int[size];
 	}
 	
+	// Insert a value to the stack
 	public void push(int x) {
 		if(!isFull()) {
-			count++;
+			count++; // increase the size
 			stackArr[count] = x;
-		} else {
+		} else { // if its full error out 
 			System.err.println("Stack is Full");
 		}
 	}
 	
+	// Get the last value from the stack
 	public int pop() {
 		int temp = -1;
 		if(!isEmpty()) {
 			temp = stackArr[count];
-			count--;
-		} else {
+			count--; // decrease the size
+		} else { // if its empty error out 
 			System.err.println("Stack is Empty");
 		}
 		return temp;
 	}
 	
+	// check to see the stack is empty
 	public boolean isEmpty() {
 		return (count == -1);
 	}
 
+	// check to see the stack is full
 	public boolean isFull() {
 		return (stackArr.length == count);
 	}
