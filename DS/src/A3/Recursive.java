c0ed3435556175b3cc4632fffd0327569beaa394
Ramasubbaiya Adaikkalam <ramasubbaiya@gmail.com>
2018-02-27 23:52:40 -0500

    Recursion with memoization

--- DS/src/A3/Recursive.java
+++ DS/src/A3/Recursive.java
@@ -10,19 +10,37 @@ public class Recursive {
 		return decrement(n-1);
 	}
 	
+	/**
+	 * DISADVANTAGES OF THIS APPROACH
+	 * 
+	 * The result is stored in int which can handle only a first 48 fibonacci numbers, 
+	 * after this the integer fill minus bit and result is wrong.
+	 * 
+	 * 	You never can run fibonacci(50)
+	 *  The problem is that the it calls fibonacci not 50 times but much more.
+	 * 
+	 * At first it calls fibonacci(49)+fibonacci(48),
+	 * next fibonacci(48)+fibonacci(47) and fibonacci(47)+fibonacci(46)
+	 * Each time it became fibonacci(n) worse, so the complexity is exponential.
+	 * 
+	 * FIX WITH MEMOIZATION
+	 */
+	static int count;
 	public static int fibonacci(int n) {
-		System.out.println(n);
-		if(n == 1) {
-			return 1;
-		}
+		count++;
 		if(n == 0) {
 			return 0;
 		}
-		return n = fibonacci(n-1) * fibonacci(n-2);
+		else if(n == 1) {
+			return 1;
+		} else {
+			return fibonacci(n-1) + fibonacci(n-2);
+		}
 	}
 	
 	public static void main(String[] args) {
 		System.out.println(decrement(10));
 		System.out.println(fibonacci(10));
+		System.out.println(count);
 	}
 }
--- DS/src/A3/RecursiveWithMemoization.java
+++ DS/src/A3/RecursiveWithMemoization.java
@@ -2,4 +2,35 @@ package A3;
 
 public class RecursiveWithMemoization {
 
+	// count param to know the no. of iteration
+	static int count = 0;
+	
+	// Fibonacci with memorization of pre-calculated data
+	public static int fibonacci(int n) {
+		int[] fibnacciArr = new int[100];
+		System.out.println(n);
+		if(fibnacciArr[n] == 0) {
+			count++;
+			if(n == 0) {
+				fibnacciArr[0] = 0;
+				return 0;
+			} else if(n == 1) {
+				fibnacciArr[1] = 1;
+				return 1;
+			} else {
+				fibnacciArr[n] = fibonacci(n-1) + fibonacci(n-2);
+				return fibnacciArr[n];
+			}
+		} else {
+			System.out.println("sdsd");
+			return fibnacciArr[n];
+		}
+	}
+	
+	
+	// Testing the outputs
+	public static void main(String[] args) {
+		System.out.println(fibonacci(10));
+		System.out.println(count);
+	}
 }
