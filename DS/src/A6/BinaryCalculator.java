bc36ee985a1d54f8dba5c8ad0d840dd6d655e74e
Ramasubbaiya Adaikkalam <ramasubbaiya@gmail.com>
2018-03-28 23:31:27 -0400

    Adding initial methods

--- DS/src/A6/BinaryCalculator.java
+++ DS/src/A6/BinaryCalculator.java
@@ -2,4 +2,26 @@ package A6;
 
 public class BinaryCalculator {
 
+	// Get two binary values and add
+	private static void add() {
+		// TODO Auto-generated method stub
+
+	}
+	
+	// Get two binary values and subtract
+	private static void subtract() {
+		// TODO Auto-generated method stub
+
+	}
+	
+
+	private static void divide() {
+		// TODO Auto-generated method stub
+
+	}
+	
+	// Get two binary values and multiply
+	private static void multiply() {
+		// TODO Auto-generated method stub		
+	}
 }
